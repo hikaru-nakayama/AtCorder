9a675a17e24c98c81cf7eedb36dfb8da45fb33fe
nakayama <nakayama@nakayamanoMacBook-Air.local>
2025-07-12 18:32:35 +0900

    format

--- typical90/030.cpp
+++ typical90/030.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
+
 #include <vector>
 
 using namespace std;
 
 int main() {
-	int N, K;
-	cin >> N >> K;
-	vector<int> primes(N+1, 0);
+  int N, K;
+  cin >> N >> K;
+  vector<int> primes(N + 1, 0);
 
-	// エラトステネスの篩
-	// N * 調和級数なので計算量は NlogN
-	for ( int i = 2; i <= N; i ++ ) {
-		if ( primes[i] > 0 ) continue;
-		int m = N / i;
-		for ( int j = 1; j <= m; j ++ ) {
-			primes[j * i] ++;
-		}
-	}
+  // エラトステネスの篩
+  // N * 調和級数なので計算量は NlogN
+  for (int i = 2; i <= N; i++) {
+    if (primes[i] > 0)
+      continue;
+    int m = N / i;
+    for (int j = 1; j <= m; j++) {
+      primes[j * i]++;
+    }
+  }
 
-	int ans = 0;
-	for ( int i = 2; i <= N; i ++) {
-		if ( primes[i] >= K ) {
-			ans ++;
-		}
-	}
+  int ans = 0;
+  for (int i = 2; i <= N; i++) {
+    if (primes[i] >= K) {
+      ans++;
+    }
+  }
 
-	cout << ans << endl;
+  cout << ans << endl;
 }
