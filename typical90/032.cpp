8ef33b19bf2339c025711ab1b833d9663f9b73d2
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2025-07-13 21:50:14 +0900

    typical-032

--- typical90/032.cpp
+++ typical90/032.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+#define INF (1ll << 60)
+
 int main() {
   int N;
   cin >> N;
@@ -19,9 +21,9 @@ int main() {
   int M;
   cin >> M;
 
-  vector<vector<int>> Rumor;
+  vector<vector<int>> Rumor(N + 1);
 
-  vector<int> v;
+  vector<int> v(N);
 
   for (int i = 0; i < N; i++) {
     v[i] = i + 1;
@@ -34,6 +36,28 @@ int main() {
     Rumor[y].push_back(x);
   }
 
+  long long ans = INF;
   do {
+    long long result = 0;
+    for (int i = 0; i < N; i++) {
+      vector<int> bad = Rumor[v[i]];
+      for (auto b : bad) {
+        if (i + 1 < N && b == v[i + 1]) {
+          result = -1;
+        }
+      }
+      if (result != -1) {
+        result += A[v[i]][i + 1];
+      }
+    }
+    if (result != -1) {
+      ans = min(ans, result);
+    }
   } while (next_permutation(v.begin(), v.end()));
+
+  if (ans != INF) {
+    cout << ans << endl;
+  } else {
+    cout << -1 << endl;
+  }
 }
